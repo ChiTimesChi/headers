d8867ddf221cbcf354bc990df75398f19930444e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-22 17:14:47 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# headers
+
+Generate perfect code headers every time.
+
+## Setup
+
+```sh
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+```sh
+$ python3 headers.py this is quite a pretty good looking box
+```
+
+```solidity
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║               THIS IS QUITE A PRETTY GOOD LOOKING BOX                ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+```
+
+It will also copy the header to your clipboard automatically.
+
+### With VSCode
+
+- Set your global `tasks.json` like so to add the command as task.
+  > Change `~/VSCodeProjects/headers/headers.py` to the actual path to the script.
+
+```json
+{
+  "version": "2.0.0",
+  "tasks": [
+    {
+      "label": "Generate Header",
+      "type": "shell",
+      "command": "python3 ~/VSCodeProjects/headers/headers.py ${input:header}",
+      "presentation": {
+        "reveal": "never"
+      }
+    }
+  ],
+  "inputs": [
+    {
+      "id": "header",
+      "description": "Header",
+      "type": "promptString"
+    }
+  ]
+}
+```
+
+- To really speed-up your workflow, you can even add a keybind for the task in `keybindings.json`:
+  > Feel free to use `"cmd+<...>"`, if you happen to own one of these fancy Mac computers.
+
+```json
+[
+  {
+    "key": "ctrl+alt+h",
+    "command": "workbench.action.tasks.runTask",
+    "args": "Generate Header"
+  }
+]
+```
+
+## Credits
+
+Inspired (just a bit more than straight up copypasta fork) by transmissions11's [`headers`](https://github.com/transmissions11/headers),
+which was in turn inspired by virtualjpeg's [`blocky`](https://github.com/virtualjpeg/blocky).
---
+++ headers.py
@@ -0,0 +1,75 @@
+import sys
+import pyperclip
+
+# https://en.wikipedia.org/wiki/Box-drawing_character
+SYMBOL_HEAD: str = "═"
+SYMBOL_BUTT: str = "═"
+
+# Comment section needs to start with /*
+PRE_HEAD: str = "/*╔"
+PRE_BODY: str = "▏*║"
+PRE_BUTT: str = "\*╚"
+
+# "\\" is one symbol: escaped \
+POST_HEAD: str = "╗*\\"
+POST_BODY: str = "║*▕"
+POST_BUTT: str = "╝*/"
+# Comment section needs to end with */
+
+# Total width (including tabulation) for headers
+WIDTH: int = 80
+
+# Amount of spaces used for tabulation
+TAB: int = 4
+# Set this to False, if you use TAB symbol for tabulating. I won't judge you.
+TAB_SPACE: bool = True
+
+# Set this to True, if you want the head line to be tabbed.
+HEAD_TAB: bool = False
+# Set this to True, if you want the rest of the lines to be tabbed.
+REST_TAB: bool = True
+# Set this to True, if you want the line following the headers to be tabbed.
+ADD_EXTRA_TAB: bool = True
+
+
+if __name__ == "__main__":
+    # Extract args, skip the first one: it is "headers.py"
+    args = sys.argv[1:]
+
+    # Check that all decorators have the same width
+    assert len(PRE_HEAD) == len(PRE_BODY) == len(PRE_BUTT)
+    assert len(POST_HEAD) == len(POST_BODY) == len(POST_BUTT)
+
+    # Check head/butt decorator
+    assert len(SYMBOL_HEAD) == len(SYMBOL_BUTT) == 1
+
+    # Text width is total width, minus tab, minus decorators
+    width: int = WIDTH - TAB - len(PRE_BODY) - len(POST_BODY)
+
+    # Concatenate all args, using space as deliminator
+    # len(args) == 0 will lead to drawing a pretty, yet empty, box
+    title: str = (" ".join(args)).upper()
+
+    tab: str = " " * TAB if TAB_SPACE else "\t"
+    # Tabulation used for the head line
+    tab_h: str = tab if HEAD_TAB else ""
+    # Tabulation used for the rest of the lines
+    tab_r: str = tab if REST_TAB else ""
+    
+    # Construct decorators
+    head: str = SYMBOL_HEAD * width
+    butt: str = SYMBOL_BUTT * width
+
+    # Body is title string, centered
+    body: str = f"{title:^{width}}"
+
+    # The merge
+    headers: str = \
+        f"{tab_h}{PRE_HEAD}{head}{POST_HEAD}\n"\
+        f"{tab_r}{PRE_BODY}{body}{POST_BODY}\n"\
+        f"{tab_r}{PRE_BUTT}{butt}{POST_BUTT}\n"\
+        f"{tab_r if ADD_EXTRA_TAB else ''}"
+
+    # Add extra tab, if first line is not tabbed, for prettier console look.
+    print((tab if REST_TAB and not HEAD_TAB else "") + headers)
+    pyperclip.copy(headers)
